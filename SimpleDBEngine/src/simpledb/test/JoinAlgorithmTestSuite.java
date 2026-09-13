d18e4fb9089f6ae9980c8645a1b8412384c85133
Dumboiroy <alroylohzenyang@gmail.com>
2026-09-13 17:24:22 +0800

    Edit test to use Large Student DB
    - Indexing isn't preferred unless block accesses for index < merge. This
      only happens when student table is very large.

--- SimpleDBEngine/src/simpledb/test/JoinAlgorithmTestSuite.java
+++ SimpleDBEngine/src/simpledb/test/JoinAlgorithmTestSuite.java
@@ -23,7 +23,7 @@ import simpledb.server.SimpleDB;
 public class JoinAlgorithmTestSuite {
    public static void main(String[] args) {
       try {
-         SimpleDB db = new SimpleDB("studentdb");
+         SimpleDB db = new SimpleDB("largestudentdb");
          Transaction tx = db.newTx();
          Planner planner = db.planner();
 
@@ -31,15 +31,15 @@ public class JoinAlgorithmTestSuite {
 
          System.out.println("=== Test 1: index join (STUDENT.MajorId = DEPT.DId) ===");
          String q1 = "select sname, dname from student, dept where majorid = did";
-         allPassed &= runAndCheck(planner, tx, q1, 9);
+         allPassed &= runAndCheck(planner, tx, q1, 500);
 
          System.out.println("\n=== Test 2: merge join (COURSE.DeptId = DEPT.DId) ===");
          String q2 = "select title, dname from course, dept where deptid = did";
-         allPassed &= runAndCheck(planner, tx, q2, 6);
+         allPassed &= runAndCheck(planner, tx, q2, 7);
 
          System.out.println("\n=== Test 3: nested-loops join (STUDENT.SId != ENROLL.StudentId) ===");
          String q3 = "select sname, grade from student, enroll where sid != studentid";
-         allPassed &= runAndCheck(planner, tx, q3, 48);
+         allPassed &= runAndCheck(planner, tx, q3, 623750);
 
          System.out.println("\n=== OVERALL: " + (allPassed ? "ALL TESTS PASSED" : "SOME TESTS FAILED") + " ===");
 
